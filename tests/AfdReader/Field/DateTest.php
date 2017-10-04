cdb210524aa57467512d4ed4e1b46dc73a2180f5
Alex Assunção <alex.assuncao@convenia.com.br>
2017-10-04 14:12:18 -0300

    fix imports and style

--- tests/AfdReader/Field/DateTest.php
+++ tests/AfdReader/Field/DateTest.php
@@ -16,4 +16,11 @@ class DateTest extends PHPUnit_Framework_TestCase
         $val = $obj->format('13061988');
         $this->assertInstanceOf('DateTime', $val);
     }
+
+    public function test_fail_value()
+    {
+        $this->setExpectedException('Convenia\AfdReader\Exception\InvalidDateFormatException');
+        $obj = new Date();
+        $obj->format('225');
+    }
 }
